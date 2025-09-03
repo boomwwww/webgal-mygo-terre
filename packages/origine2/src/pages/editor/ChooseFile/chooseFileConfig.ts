427645fbf280a8dcea29f7a46c957f91850e6eb8
KonshinHaoshin <konshinhaoshin@gmail.com>
2025-09-03 17:50:20 +0800

    ä¿åå修复前导点

--- packages/origine2/src/pages/editor/ChooseFile/chooseFileConfig.ts
+++ packages/origine2/src/pages/editor/ChooseFile/chooseFileConfig.ts
@@ -3,7 +3,7 @@ export const extNameMap = new Map<IExtNameType, string[]>([]);
 
 extNameMap.set('scene', ['.txt']);
 extNameMap.set('image', ['.png', '.jpg', '.jpeg', '.webp','.webm','.mov','.gif']);
-extNameMap.set('video', ['.mp4', '.webm', '.mkv','mov']);
+extNameMap.set('video', ['.mp4', '.webm', '.mkv','.mov']);
 extNameMap.set('audio', ['.mp3', '.ogg', '.wav','.flac']);
 extNameMap.set('tex', ['.png', '.webp']);
 extNameMap.set('json', ['.json','.jsonl']);
